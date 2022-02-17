18cde56d56d125edcd2e8846a14fe65dfbfaecb6
spark-156 <lucabergman@hotmail.nl>
2022-02-17 14:57:57 +0100

    test api call

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="Feeby",
     version=1,
     root_path="/api/v1"
 )
+
+@app.post("/launch")
+async def launch(request: Request):
+    return "launched"   
 
 @app.get("/{update_id}")
 async def root(update_id: str):
