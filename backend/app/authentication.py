5dbb20c7cfdcf6b5f1bd0879d3e0d2d0d0d72c19
spark-156 <lucabergman@hotmail.nl>
2022-02-22 11:23:00 +0100

    move redir auth call

---
+++ backend/app/authentication.py
@@ -0,0 +1,13 @@
+from fastapi import responses, status
+
+from config import BASE_URL, CLIENT_ID, BASE_APP_API_URL
+
+def redir_to_oauth():
+  """
+  Redirects the user to Oauth2 flow from canvas
+  """
+  print("Redirecting to auth")
+  return responses.RedirectResponse(
+    f'{BASE_URL}/login/oauth2/auth?client_id={CLIENT_ID}&response_type=code&state=aaaaaa&redirect_uri={BASE_APP_API_URL}/callback',
+    status_code=status.HTTP_302_FOUND
+    )
