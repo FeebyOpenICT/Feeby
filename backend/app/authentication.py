008bd723ae8b0d4d406061661864ce3bce6530af
spark-156 <lucabergman@hotmail.nl>
2022-02-22 12:41:56 +0100

    fix redirect to auth

--- backend/app/authentication.py
+++ backend/app/authentication.py
@@ -1,6 +1,6 @@
 from fastapi import responses, status
 
-from config import BASE_URL, CLIENT_ID, BASE_APP_API_URL
+from config import BASE_URL, CLIENT_ID, BASE_APP_API_URL, DEVELOPER_KEY, DELEVOPER_KEY_ID
 
 def redir_to_oauth():
   """
@@ -8,6 +8,6 @@ def redir_to_oauth():
   """
   print("Redirecting to auth")
   return responses.RedirectResponse(
-    f'{BASE_URL}/login/oauth2/auth?client_id={CLIENT_ID}&response_type=code&state=aaaaaa&redirect_uri={BASE_APP_API_URL}/callback',
-    status_code=status.HTTP_302_FOUND
+      f'{BASE_URL}/login/oauth2/auth?client_id={DELEVOPER_KEY_ID}&response_type=code&state=aaaaaa&redirect_uri={BASE_APP_API_URL}/callback',
+      status_code=status.HTTP_302_FOUND
     )
--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,11 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-CLIENT_ID = os.environ["LTI_CONSUMER_KEY"]
-CLIENT_SECRET = os.environ["LTI_SHARED_SECRET"]
-DEVELOPER_KEY = os.environ["DEVELOPER_KEY"]
-
+CLIENT_ID = "laskjhfdlkjh"
+CLIENT_SECRET = "TWO"
+DEVELOPER_KEY = "XmF8czbHavW1YooJCQZJ3Z5YhajtsNaDHslxtZc7xi9ieiAWPmV2vZPuPTOnLibU"
+DELEVOPER_KEY_ID = "34"
 # Canvas instance
-BASE_URL = os.environ["BASE_URL"]
+BASE_URL = "https://canvas.lucabergman.nl"
 
 # Hosted LTI instance
-BASE_APP_URL = os.environ["BASE_APP_URL"]
+BASE_APP_URL = "http://localhost"
 BASE_APP_API_URL = f'{BASE_APP_URL}' # make /api/v1 dynamic so it can be set to something else
 
