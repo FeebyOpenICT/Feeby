9220b30528af5b43275e91cab6011693aa707091
spark-156 <lucabergman@hotmail.nl>
2022-02-22 11:22:46 +0100

    make all os.environ calls throw keyerrors if no value is set

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,11 @@
 import os
 
-CLIENT_ID = os.environ.get("LTI_CONSUMER_KEY")
-CLIENT_SECRET = os.environ.get("LTI_SHARED_SECRET")
+CLIENT_ID = os.environ["LTI_CONSUMER_KEY"]
+CLIENT_SECRET = os.environ["LTI_SHARED_SECRET"]
 
 # Canvas instance
-BASE_URL = os.environ.get("BASE_URL")
+BASE_URL = os.environ["BASE_URL"]
 
 # Hosted LTI instance
-BASE_APP_URL = os.environ.get("BASE_APP_URL")
-BASE_APP_API_URL = BASE_APP_URL + '/api/v1' # make /api/v1 also an env. var. so it can be set to /api/v2 if ever needed
+BASE_APP_URL = os.environ["BASE_APP_URL"]
+BASE_APP_API_URL = f'{BASE_APP_URL}/api/v1' # make /api/v1 dynamic so it can be set to something else
