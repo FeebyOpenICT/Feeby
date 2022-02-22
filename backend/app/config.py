19312f59c72b4ca3d4f270ff8ed94840e1e70c6d
spark-156 <lucabergman@hotmail.nl>
2022-02-22 11:39:43 +0100

    add dev key

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,11 +1,17 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 CLIENT_ID = os.environ["LTI_CONSUMER_KEY"]
 CLIENT_SECRET = os.environ["LTI_SHARED_SECRET"]
+DEVELOPER_KEY = os.environ["DEVELOPER_KEY"]
 
 # Canvas instance
 BASE_URL = os.environ["BASE_URL"]
 
 # Hosted LTI instance
 BASE_APP_URL = os.environ["BASE_APP_URL"]
-BASE_APP_API_URL = f'{BASE_APP_URL}/api/v1' # make /api/v1 dynamic so it can be set to something else
+BASE_APP_API_URL = f'{BASE_APP_URL}' # make /api/v1 dynamic so it can be set to something else
+
