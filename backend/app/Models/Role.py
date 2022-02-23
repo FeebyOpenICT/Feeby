0773427502d6496ddf965d340b4dfb184e5c9c94
spark-156 <lucabergman@hotmail.nl>
2022-02-23 14:14:04 +0100

    add nullable fields to models

--- backend/app/Models/Role.py
+++ backend/app/Models/Role.py
@@ -5,9 +5,9 @@ from database import Base
 class Role(Base):
   __tablename__ = 'role'
 
-  id = Column(Integer, primary_key=True)
-  title = Column(String(length=255))
-  description = Column(String(length=1000))
+  id = Column(Integer, primary_key=True, nullable=False)
+  title = Column(String(length=255), nullable=False)
+  description = Column(String(length=1000), nullable=False)
 
   def __repr__(self) -> str:
     return f"<Role title={self.title} description={self.description}>"
--- backend/app/Models/User.py
+++ backend/app/Models/User.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey 
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
 from .Role import Base
@@ -6,11 +6,13 @@ from .Role import Base
 class User(Base):
   __tablename__ = 'user'
 
-  id = Column(Integer, primary_key=True)
-  fullname = Column(String(length=255))
-  email = Column(String(length=255), unique=True)
-  canvas_id = Column(Integer, unique=True)
-  access_token = Column(String)
+  id = Column(Integer, primary_key=True, nullable=False)
+  fullname = Column(String(length=255), nullable=False)
+  canvas_email = Column(String(length=255), nullable=False, unique=True)
+  canvas_id = Column(Integer, nullable=False, unique=True)
+  access_token = Column(String, nullable=True)
+  refresh_token = Column(String, nullable=True)
+  expires_at = Column(Date, nullable=True)
 
   role_id = Column(Integer, ForeignKey('role.id'))
   role = relationship('Role')
