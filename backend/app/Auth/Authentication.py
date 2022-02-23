97d00485918d5437679583ddaa19ea34ea9a86be
spark-156 <lucabergman@hotmail.nl>
2022-02-23 20:20:26 +0100

    create user and test access token

--- backend/app/Auth/Authentication.py
+++ backend/app/Auth/Authentication.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, Form, Depends
+from sqlalchemy.orm import Session
 import requests
 
 from Exceptions.AuthenticationException import OAuth2AuthenticationException
-from config import BASE_URL, DELEVOPER_KEY_ID, DEVELOPER_KEY, BASE_APP_API_CALLBACK_URL
 
+from database import get_db_connection
+from config import BASE_URL, DELEVOPER_KEY_ID, DEVELOPER_KEY, BASE_APP_API_CALLBACK_URL
+from Models.User import User, Student
 
 router = APIRouter(
   prefix="/auth",
@@ -11,7 +14,7 @@ router = APIRouter(
 )
 
 @router.get("/callback")
-async def callback(code: str = None, error: str = None, error_description: str = None):
+async def callback(code: str = None, error: str = None, error_description: str = None, db: Session = Depends(get_db_connection)):
     """
     Callback for oauth2 flow https://canvas.instructure.com/doc/api/file.oauth.html
     """
@@ -33,8 +36,35 @@ async def callback(code: str = None, error: str = None, error_description: str =
         if r.status_code == 500:
             # Canceled oauth or server error
             raise OAuth2AuthenticationException(status_code=500, message="An error occured on the canvas authentication servers. Please refresh the page and try again. Contact support if the issue persists.")
+
         json = r.json()
-        return json
+
+        """ json ^
+        {
+            "access_token": str,
+            "token_type": "Bearer",
+            "user": {
+                "id": 1, 
+                "name": str
+            },
+            "refresh_token": str,
+            "expires_in": int
+        }
+        """
+
+        # check if user already exists in db
+        user = db.query(User).filter(User.canvas_id == json["user"]["id"]).first()
+
+        if user:
+            # user found
+            return user
+        else:
+            # no user found > create new one
+            self = requests.get(f'{BASE_URL}/api/v1/users/self', headers={ 'Authorization': f'Bearer {json["access_token"]}' })
+            self_json = self.json()
+            new_user = Student(db=db, fullname=self_json["name"], canvas_id=json["user"]["id"])
+            return new_user.save_self(db)
+
     else:
         raise OAuth2AuthenticationException()
 
