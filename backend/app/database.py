fcd672da24e71865231d38165b8c2211a385fa91
spark-156 <lucabergman@hotmail.nl>
2022-02-23 12:48:43 +0100

    create database connection and dependency

---
+++ backend/app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://feeby:hallo123@localhost:5432/feeby"
+
+engine = create_engine(
+  SQLALCHEMY_DATABASE_URL,
+  echo=True
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db_connection():
+  db = SessionLocal()
+  try:
+      yield db
+  finally:
+      db.close()
