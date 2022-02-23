fa7430a5a589a1e2efbb98924179a6b77c782ad5
spark-156 <lucabergman@hotmail.nl>
2022-02-23 15:24:43 +0100

    add custom init functions and get_student_role function

--- backend/app/Models/Role.py
+++ backend/app/Models/Role.py
@@ -1,13 +1,32 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
 from database import Base
 
 class Role(Base):
   __tablename__ = 'role'
 
   id = Column(Integer, primary_key=True, nullable=False)
-  title = Column(String(length=255), nullable=False)
-  description = Column(String(length=1000), nullable=False)
+  title: str = Column(String(length=255), nullable=False, unique=True, index=True)
+  description: str = Column(String(length=1000), nullable=False)
+
+  def __init__(self, title, description) -> None:
+    self.title = title
+    self.description = description
+    super().__init__()
 
   def __repr__(self) -> str:
     return f"<Role title={self.title} description={self.description}>"
+
+  def get_student_role(db: Session):
+    """
+    Gets the student role object mapping
+
+    db = db connection session
+    """
+    student_role = db.query(Role).filter(Role.title == 'student').first()
+    if not student_role:
+      # no student role in db, create one
+      student_role = Role(title="student", description="I am a student")
+      db.add(student_role)
+      db.commit()
+    return student_role
--- backend/app/Models/User.py
+++ backend/app/Models/User.py
@@ -1,3 +1,4 @@
+from datetime import date
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
@@ -9,7 +10,7 @@ class User(Base):
   id = Column(Integer, primary_key=True, nullable=False)
   fullname = Column(String(length=255), nullable=False)
   canvas_email = Column(String(length=255), nullable=False, unique=True)
-  canvas_id = Column(Integer, nullable=False, unique=True)
+  canvas_id = Column(Integer, nullable=False, unique=True, index=True)
   access_token = Column(String, nullable=True)
   refresh_token = Column(String, nullable=True)
   expires_at = Column(Date, nullable=True)
@@ -17,5 +18,23 @@ class User(Base):
   role_id = Column(Integer, ForeignKey('role.id'))
   role = relationship('Role')
 
+  def __init__(
+    self, 
+    fullname: str, 
+    canvas_email: str, 
+    canvas_id: int, 
+    access_token: str = None, 
+    refresh_token: str = None,
+    expires_at: date = None,
+    **kwargs
+  ) -> None:
+    self.fullname = fullname
+    self.canvas_email = canvas_email
+    self.canvas_id = canvas_id
+    self.access_token = access_token
+    self.refresh_token = refresh_token
+    self.expires_at = expires_at
+    super().__init__(**kwargs)
+
   def __repr__(self) -> str:
-    return f"<User id={self.id} email={self.email} role={self.role} fullname={self.fullname} canvas_id={self.canvas_id}>"
+    return f"<User id={self.id} canvas_email={self.canvas_email} role={self.role} fullname={self.fullname} canvas_id={self.canvas_id}>"
