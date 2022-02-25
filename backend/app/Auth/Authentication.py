34c9ac075ecd4cd76e22a6c45ccd1b048307a8cd
spark-156 <lucabergman@hotmail.nl>
2022-02-25 09:18:58 +0100

    refresh auth call

--- backend/app/Auth/Authentication.py
+++ backend/app/Auth/Authentication.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Form, Depends
+from typing import Optional
+from fastapi import APIRouter, Cookie, Depends, Response
 from sqlalchemy.orm import Session
 import requests
 
 from Exceptions.AuthenticationException import OAuth2AuthenticationException
+from Auth import redir_to_auth
 
 from database import get_db_connection
 from config import BASE_URL, DELEVOPER_KEY_ID, DEVELOPER_KEY, BASE_APP_API_CALLBACK_URL
@@ -14,7 +16,7 @@ router = APIRouter(
 )
 
 @router.get("/callback")
-async def callback(code: str = None, error: str = None, error_description: str = None, db: Session = Depends(get_db_connection)):
+async def callback(response: Response, code: str = None, error: str = None, error_description: str = None, db: Session = Depends(get_db_connection)):
     """
     Callback for oauth2 flow https://canvas.instructure.com/doc/api/file.oauth.html
     """
@@ -56,22 +58,46 @@ async def callback(code: str = None, error: str = None, error_description: str =
         """
 
         # check if user already exists in db
-        user = db.query(User).filter(User.canvas_id == json["user"]["id"]).first()
+        user = User.get_user_by_canvas_id(json["user"]["id"], db)
 
-        if user:
-            # user found
-            return user
-        else:
-            # no user found > create new one
+        if not user:
+            # no user found > create new one and save in db
             self = requests.get(f'{BASE_URL}/api/v1/users/self', headers={ 'Authorization': f'Bearer {json["access_token"]}' })
             self_json = self.json()
             new_user = Student(db=db, fullname=self_json["name"], canvas_id=json["user"]["id"])
-            return new_user.save_self(db)
+            new_user.save_self(db)
+
+        return user
 
     else:
         raise OAuth2AuthenticationException()
 
 
-#TODO refresh token call
+@router.get("/refresh")
+async def refresh_token(refresh_token: Optional[str] = Cookie(None)):
+    """
+    Refresh call for getting a new access token from canvas
+    """
+    if not refresh_token:
+        return redir_to_auth()
 
-#TODO decorator/ global middleware to check access token on each api call
+    r = requests.post(f'{BASE_URL}/login/oauth2/token', data={
+            'grant_type': 'refresh_token',
+            'client_id': DELEVOPER_KEY_ID,
+            'client_secret': DEVELOPER_KEY,
+            'refresh_token': refresh_token
+        })
+
+    # got the access_token, now check for errors:
+    if r.status_code >= 400:
+        if r.status_code == 500:
+            # Canceled oauth or server error
+            raise OAuth2AuthenticationException(status_code=500, message="An error occured on the canvas authentication servers. Please refresh the page and try again. Contact support if the issue persists.")
+        else:
+            raise OAuth2AuthenticationException(status_code=r.status_code)
+
+    # TODO maybe check user in db?
+
+    return r.json()
+
+#TODO decorator/ global middleware to check access token on each api call 
