b272523772b20276794c28e98378c99ba083108c
spark-156 <lucabergman@hotmail.nl>
2022-03-09 13:49:59 +0100

    test api call

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from Exceptions.AuthenticationException import OAuth2AuthenticationException, oauth2_authentication_exception_handler
 from Auth import Authentication
-# from Auth.valid_user import CheckValidUser
 from LTI import lti
+from Auth.validate_user import get_current_active_user
 
 from database import engine
 
@@ -24,13 +24,14 @@ app = FastAPI(
 
 app.add_exception_handler(OAuth2AuthenticationException, oauth2_authentication_exception_handler)
 
-# app.add_middleware(CheckValidUser)
-
 
 app.include_router(lti.router)
 
-
 app.include_router(Authentication.router)
+ 
+@app.get("/users/self")
+async def read_users_me(current_user = Depends(get_current_active_user)):
+    return current_user
 
 if __name__ == "__main__":
     uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, workers=1)
