5f2d30716583b784c178b1e90bb7c50d7b4e7cca
lucius02 <54938206+lucius02@users.noreply.github.com>
2022-03-11 10:49:52 +0100

    CArd component made, border not right yet.

---
+++ frontend/components/CardCom.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card
+    class="postcard"
+  >
+    <div id="header" class="header">
+      <v-card-title>Post Card</v-card-title>
+      <v-card-subtitle>11-03-2022</v-card-subtitle>
+    </div>
+    <v-card-text class="body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Proin laoreet sapien at nibh luctus accumsan.
+      Sed accumsan sem nulla, venenatis lacinia lacus pellentesque eget.
+      Mauris vitae efficitur nisl.
+      Ut mattis nisl ut rutrum commodo.
+      Ut in tellus nunc.
+      In venenatis, arcu ultrices condimentum vulputate, tortor purus ultrices tellus, eget congue nunc nunc in justo.
+      Nulla at bibendum magna.
+      Curabitur molestie a tortor non tempor.
+      Nulla nunc justo, facilisis ut diam eu, condimentum ultricies augue.
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardCom'
+}
+</script>
+
+<style scoped>
+  .postcard{
+      width: 90vw;
+      height: 20vh;
+      position: relative;
+      left: 5vw;
+      border-style: hidden solid solid;
+      border-width: 1px;
+      border-color: black;
+  }
+  .header{
+      background-color: #0079CF;
+      color: white;
+  }
+</style>
--- frontend/components/HomePage.vue
+++ frontend/components/HomePage.vue
@@ -4,6 +4,7 @@
     <div class="testpage">
       <h3>FEEBY</h3>
       <p>hello</p>
+      <CardCom />
     </div>
     <FooterCom />
   </div>
@@ -12,16 +13,16 @@
 <script>
 import HeaderCom from './HeaderCom.vue'
 import FooterCom from './FooterCom.vue'
+import CardCom from './CardCom.vue'
 
 export default {
   name: 'HomePage',
-  components: { HeaderCom, FooterCom }
+  components: { HeaderCom, FooterCom, CardCom }
 }
 </script>
 
 <style scoped>
 .testpage{
   height: 100vw;
-  z-index: 1;
 }
 </style>
