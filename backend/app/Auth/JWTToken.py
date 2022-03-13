25c7226f775d35dc55ee62f17a7f77c18d912d61
spark-156 <lucabergman@hotmail.nl>
2022-03-13 13:52:02 +0100

    update access token

--- backend/app/Auth/JWTToken.py
+++ backend/app/Auth/JWTToken.py
@@ -5,13 +5,16 @@ import requests
 
 from config import JWT_ALGORITHM, JWT_SECRET, BASE_URL
 
+class AccessToken:
+  """
+  JWT Token class for encoding, decoding and validating
 
-class RolesToken:
+  Not in database but can decode, encode and validate itself against canvas
   """
-  JWT Token class for encoding and decoing the roles passed to the tool at lti launch
-  """
-  def __init__(self, roles) -> None:
-    # TODO decode canvas roles to simpler understood roles
+  def __init__(self, access_token: str, refresh_token: str, canvas_id: int, roles: List[str]) -> None:
+    self.access_token = access_token
+    self.refresh_token = refresh_token
+    self.canvas_id = canvas_id
     """
     All roles ive found so far:
 
@@ -23,40 +26,13 @@ class RolesToken:
     urn:lti:instrole:ims/lis/Student,
     urn:lti:role:ims/lis/Learner,
     urn:lti:sysrole:ims/lis/User
+
+    TODO 
     """
     self.roles = roles
 
-  @property
-  def encoded_token(self):
-    """
-    encoded jwt token
-    """
-    return jwt.encode({
-      "roles": self.roles
-    }, JWT_SECRET, algorithm=JWT_ALGORITHM)
-  
-  # def get_roles_from_token(self, token):
-  #   """
-  #   Gets the roles from an encoded token
-
-  #   returns a list of strings including all roles
-  #   """
-  #   jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-
-class AccessToken:
-  """
-  JWT Token class for encoding, decoding and validating
-
-  Not in database but can decode, encode and validate itself against canvas
-  """
-  def __init__(self, access_token: str, refresh_token: str, canvas_id: int, scopes: List[str]) -> None:
-    self.access_token = access_token
-    self.refresh_token = refresh_token
-    self.canvas_id = canvas_id
-    self.scopes = scopes
-
   def __repr__(self) -> str:
-    return f"canvas_id={self.canvas_id} access_token={self.access_token} refresh_token={self.refresh_token} scopes={self.scopes}>"
+    return f"canvas_id={self.canvas_id} access_token={self.access_token} refresh_token={self.refresh_token} scopes={self.roles}>"
 
   @property
   def encoded_token(self):
@@ -67,7 +43,7 @@ class AccessToken:
       "access_token": self.access_token,
       "refresh_token": self.refresh_token,
       "canvas_id": self.canvas_id,
-      "scopes": self.scopes
+      "roles": self.roles
     }, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
 
@@ -112,12 +88,12 @@ class AccessToken:
       if canvas_id is None or access_token is None:
         raise credentials_exception
 
-      token_scopes = payload.get("scopes", [])
+      roles = payload.get("roles", [])
     except (jwt.JWTError):
       raise credentials_exception
     
     return AccessToken(
-      scopes=token_scopes, 
+      scopes=roles, 
       canvas_id=canvas_id,
       access_token=access_token,
       refresh_token=refresh_token
