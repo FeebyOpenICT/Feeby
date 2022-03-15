948190b30846b399e2d59b908060a870a078aedc
spark-156 <lucabergman@hotmail.nl>
2022-03-15 14:44:57 +0100

    fix refresh call

--- backend/app/Auth/Authentication.py
+++ backend/app/Auth/Authentication.py
@@ -104,7 +104,7 @@ async def refresh_token(response: Response, jwt: HTTPAuthorizationCredentials = 
     if jwt is None:
       return HTTPException(status_code=400, detail="No jwt token was provided")
 
-    jwt_token = AccessToken.decode_token(jwt)
+    jwt_token = AccessToken.decode_token(jwt.credentials)
 
     if jwt_token.refresh_token is None:
       return HTTPException(400, "No refresh token was provided")
