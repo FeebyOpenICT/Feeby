a1d49c08cce60ea9f6b5cba1f1598241cda96876
spark-156 <lucabergman@hotmail.nl>
2022-03-15 16:03:56 +0100

    fix indentationx

--- backend/app/Schemas/Post.py
+++ backend/app/Schemas/Post.py
@@ -3,16 +3,15 @@ from datetime import datetime
 
 
 class CreatePost(BaseModel):
-    title: str
-    description: str
+  title: str
+  description: str
 
 
 class PostInDB(CreatePost):
-    id: int
-    user_id: int
-    time_created: datetime
-    time_updated: datetime
+  id: int
+  user_id: int
+  time_created: datetime
+  time_updated: datetime
 
-    class Config:
-        orm_mode = True
-
+  class Config:
+    orm_mode = True
