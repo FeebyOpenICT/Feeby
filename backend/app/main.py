e420a82249ec2a231750ca8f48bc89f2c9ae5d17
Lisa394 <123haring@gmail.com>
2022-03-15 09:59:49 +0100

    Small fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,5 +36,7 @@ app.include_router(Authentication.router)
  
 app.include_router(users.router)
 
+app.include_router(Posts.router)
+
 if __name__ == "__main__":
     uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, workers=1)
