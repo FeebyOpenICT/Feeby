ea90a7923bb1d16ad8bd223d5733b85d29874c49
timrebergen <tim.t.rebergen@student.hu.nl>
2022-03-17 10:03:40 +0100

    Homepage en index teruggezet Post maken
    Homepage en index teruggezet Post maken

---
+++ frontend/components/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <HeaderCom />
+    <div class="testpage">
+      <h3>FEEBY</h3>
+      <p>hello</p>
+    </div>
+    <FooterCom />
+  </div>
+</template>
+
+<script>
+import HeaderCom from './HeaderCom.vue'
+import FooterCom from './FooterCom.vue'
+export default {
+  name: 'HomePage',
+  components: { HeaderCom, FooterCom }
+}
+</script>
+
+<style scoped>
+.testpage{
+  height: 100vw;
+  z-index: 1;
+}
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+import HomePage from '~/components/HomePage.vue'
+export default {
+  name: 'IndexPage',
+  components: { HomePage }
+}
+</script>
