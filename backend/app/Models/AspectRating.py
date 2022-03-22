10bbc61dc41d3f73e934b9b934b8ee987ca507a2
spark-156 <lucabergman@hotmail.nl>
2022-03-22 15:08:22 +0100

    create aspect rating model

--- backend/app/Models/AspectRating.py
+++ backend/app/Models/AspectRating.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
-from .Post import Base
-from .SaveableModel import SaveableModel
+
+from Models.SaveableModel import SaveableModel
+
+from .Aspect import Base
 
 
-class Aspect(Base, SaveableModel):
+class AspectRating(Base, SaveableModel):
     """
     Mapped Aspect class
 
@@ -17,18 +19,18 @@ class Aspect(Base, SaveableModel):
     title: str = Column(String(length=255), nullable=False, index=True)
     short_description: str = Column(String(length=255), nullable=False)
     description: str = Column(String(length=1000), nullable=False)
-    external_url: str = Column(String(length=2000), nullable=True)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True),
                           server_default=func.now(), onupdate=func.now())
 
     # static not class method because I want it to always return an Aspect instance
     @staticmethod
-    def get_aspects(db: Session):
+    def get_aspect_ratings(db: Session):
         """
-        Gets aspect object mappings from db
+        Gets aspect rating object mappings from db
 
-        returns a list of aspect mapped classes, returns an empty list if none are found
+        returns a list of aspect rating mapped classes, returns an empty list if none are found
         """
-        db_aspects = db.query(Aspect).order_by(Aspect.title).all()
-        return db_aspects
+        db_aspect_ratings = db.query(
+            AspectRating).order_by(AspectRating.title).all()
+        return db_aspect_ratings
