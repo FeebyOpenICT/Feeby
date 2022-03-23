3af7b0f764960f4473e3ffa3d1839087c00be315
spark-156 <lucabergman@hotmail.nl>
2022-03-23 10:13:51 +0100

    implement aspects router in conftest

--- backend/app/conftest.py
+++ backend/app/conftest.py
@@ -12,9 +12,10 @@ from Auth import Authentication
 from LTI import lti
 from Models.Role import Role, Roles
 from Models.User import User
-from User import users
+from Users import users
 from Exceptions.NotFound import NotFound, not_found_exception_handler
 from Posts import Posts
+from Aspect import Aspects
 ######
 
 # Import the SQLAlchemy parts
@@ -104,6 +105,8 @@ def client(db):
     app.include_router(users.router)
 
     app.include_router(Posts.router)
+
+    app.include_router(Aspects.router)
 
     app.dependency_overrides[get_db_connection] = override_get_db
 
