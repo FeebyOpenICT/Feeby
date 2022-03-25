06019ed43f00b7f6114582f9dc671d7b73e1ef0e
spark-156 <lucabergman@hotmail.nl>
2022-03-25 10:38:28 +0100

    create first aspect_test

---
+++ backend/app/Models/Aspect_test.py
@@ -0,0 +1,12 @@
+from .Rating import Rating
+from .Aspect import Aspect
+
+
+def test_create_aspect_with_positional_arguments(db):
+    rating = Rating('title', 'short_desc', 'desc')
+    rating.save_self(db)
+
+    aspect = Aspect('title', 'short_desc', 'desc', 'ext_url', [rating])
+    aspect.save_self()
+
+    assert aspect.id is not None
