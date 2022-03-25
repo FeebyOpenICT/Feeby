0c34559e0bcb352b4ff368283a39284d090dad23
spark-156 <lucabergman@hotmail.nl>
2022-03-25 15:04:23 +0100

    add test test_create_post_with_positional_arguments

--- backend/app/Models/Post_test.py
+++ backend/app/Models/Post_test.py
@@ -8,3 +8,15 @@ from Models.Post import PostModel
 def test_initiate_post_without_user(db: Session):
     with pytest.raises(TypeError):
         post = PostModel("kajshdf", "kajhsdf")
+
+
+def test_create_post_with_positional_arguments(db: Session, current_active_user):
+    post = PostModel('title', 'desc', current_active_user)
+    post.save_self(db)
+
+    assert post.title == 'title'
+    assert post.description == 'desc'
+    assert post.id == 1
+    assert post.user == current_active_user
+    assert post.time_created is not None
+    assert post.time_updated is not None
