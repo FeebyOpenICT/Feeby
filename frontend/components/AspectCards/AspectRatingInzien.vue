0dff1059171c744ad573d1df7a1c0610fe99656f
lucius02 <54938206+lucius02@users.noreply.github.com>
2022-04-08 11:54:04 +0200

    aspect rating inzien lijst complete

--- frontend/components/AspectCards/AspectRatingInzien.vue
+++ frontend/components/AspectCards/AspectRatingInzien.vue
@@ -1,30 +1,45 @@
 <template>
   <v-container fluid>
-    <v-list
-      v-for="rating in ratings"
-      :key="rating.id"
+    <v-card
+      class="mx-auto"
+      max-width="400"
+      tile
     >
-      <v-list-title>
-        {{ rating.title }}
-      </v-list-title>
-      <v-list-content>
-        <v-list-text>
-          {{ rating.short_description }}
-          <br>
-          {{ rating.description }}
-        </v-list-text>
-      </v-list-content>
-    </v-list>
+      <v-list>
+        <v-list-item-group
+          multiple
+          color="#0079CF"
+        >
+          <v-list-item
+            v-for="rating in ratings"
+            :key="rating.id"
+            two-line
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="rating.title" />
+              <v-list-item-subtitle v-text="rating.short_description" />
+              <v-list-item-icon>
+                <v-icon>{{ icons.mdiPencil }}</v-icon>
+              </v-list-item-icon>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+import { mdiPencil } from '@mdi/js'
 import { axiosInstance } from '~/lib/axiosInstance'
 export default {
   name: 'AspectRatingInzien',
   data () {
     return {
-      ratings: null
+      ratings: null,
+      icons: {
+        mdiPencil
+      }
     }
   },
   mounted () {
@@ -37,5 +52,6 @@ export default {
 </script>
 
 <style scoped>
+@import '.css/styles.css';
 
 </style>
