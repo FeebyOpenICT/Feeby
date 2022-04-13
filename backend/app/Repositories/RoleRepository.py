4729e17f8e3c25ff0c4f66be968c22bd60397208
spark-156 <lucabergman@hotmail.nl>
2022-04-13 10:28:20 +0200

    create role method

--- backend/app/Repositories/RoleRepository.py
+++ backend/app/Repositories/RoleRepository.py
@@ -6,6 +6,15 @@ from Schemas.RolesEnum import RolesEnum
 
 class RoleRepository:
     @staticmethod
+    def create_role(role: RolesEnum, db: Session) -> RoleModel:
+        role = RoleModel(title=role)
+
+        db.add(role)
+        db.commit()
+
+        return role
+
+    @staticmethod
     def get_role(role: RolesEnum, db: Session):
         """
         Gets role object mapping from db
@@ -13,7 +22,5 @@ class RoleRepository:
         db_role = db.query(RoleModel).filter(
             RoleModel.title == role).first()
         if not db_role:
-            db_role = RoleModel(title=role)
-            db.add(db_role)
-            db.commit()
+            db_role = RoleRepository.create_role(role=role, db=db)
         return db_role
