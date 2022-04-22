28231cb0f399845f6361f30439b84b104ceace83
spark-156 <lucabergman@hotmail.nl>
2022-04-22 10:29:13 +0200

    import repos

--- backend/app/Repositories/__init__.py
+++ backend/app/Repositories/__init__.py
@@ -4,3 +4,5 @@ from .UserAccessPostRepository import UserAccessPostRepository
 from .UserRepository import UserRepository
 from .AspectRepository import AspectRepository
 from .RatingRepository import RatingRepository
+from .FeedbackRepository import FeedbackRepository
+from .AspectRatingRepository import AspectRatingRepository
