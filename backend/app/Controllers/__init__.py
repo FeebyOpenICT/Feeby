e9c2f68c46f2b53d3c020ced39faace85c244143
Lisa394 <55276026+Lisa394@users.noreply.github.com>
2022-04-22 12:41:00 +0200

    file controller added

--- backend/app/Controllers/__init__.py
+++ backend/app/Controllers/__init__.py
@@ -2,3 +2,4 @@ from .PostController import router as PostRouter
 from .UserController import router as UserRouter
 from .AspectController import router as AspectRouter
 from .RatingsController import router as RatingsRouter
+from .FileController import router as FileRouter
