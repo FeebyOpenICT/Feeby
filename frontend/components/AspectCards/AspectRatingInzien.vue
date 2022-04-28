99f9b9a2d50ed5749db02f95e725e801db3b9654
lucius02 <54938206+lucius02@users.noreply.github.com>
2022-04-28 10:25:48 +0200

    V-card Dialog Works, PATCH Request W.I.P

--- frontend/components/AspectCards/AspectRatingInzien.vue
+++ frontend/components/AspectCards/AspectRatingInzien.vue
@@ -1,200 +1,279 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="600"
-    max-height="700"
-    tile
-    outlined
-    elevation="2"
-    border
+  <!-- Data Tabel -->
+  <v-data-table
+    :headers="headers"
+    :items="ratings"
+    sort-by="title"
+    class="elevation-1"
+    data-app
   >
-    <!-- Vuetify Data Table Component -->
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      hide-default-footer
-      class="elevation-1"
-    >
-      <template #top>
-        <v-toolbar
-          flat
-        >
-          <v-toolbar-title>Mijn Ratings</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          />
-          <v-spacer />
+    <!-- De Header van het Tabel -->
+    <template #top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Mijn Ratings</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        />
+        <v-spacer />
 
-          <!-- Dialog Update Pop-Up -->
-          <v-overlay
-            v-model="overlay"
-            data-app
-          >
-            <!-- Update Items -->
-            <v-card light class="RatingCard">
-              <AspectRatingUpdaten />
-              <!-- Close Button -->
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="overlay = false"
-                >
-                  Cancel
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-overlay>
-        </v-toolbar>
-      </template>
-
-      <!-- Update Button -->
-      <template #[`item.actions`]>
-        <v-icon
-          small
-          class="mr-2"
-          @click="overlay = !overlay"
+        <!-- Form Dialog -->
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
         >
-          mdi-pencil
-        </v-icon>
-      </template>
-    </v-data-table>
-  </v-card>
+          <!-- New Item Button -->
+          <template #activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              New Item
+            </v-btn>
+          </template>
+
+          <!-- V-card Form -->
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.title"
+                      label="Rating Title"
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.short_description"
+                      label="Korte Beschrijving"
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.description"
+                      label="Beschrijving"
+                    />
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <!-- V-card acties -->
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="updateForm"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <!-- Een Dialog om Items te verwijderen -->
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">
+              Are you sure you want to delete this item?
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="blue darken-1" text @click="closeDelete">
+                Cancel
+              </v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">
+                OK
+              </v-btn>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <!-- Items bewerken/verwijderen knoppen -->
+    <template #[`item.actions`]="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template #no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-import AspectRatingUpdaten from './AspectRatingUpdaten.vue'
-import { axiosInstance } from '~/lib/axiosInstance'
+import { axiosInstance } from '../../lib/axiosInstance'
+import dataService from '../../lib/dataService'
+
 export default {
-  name: 'AspectRatingInzien',
-  components: { AspectRatingUpdaten },
-  data () {
-    return {
-      overlay: false,
-      absolute: true,
-      headers: [
-        { text: 'Titel', value: 'title' },
-        { text: 'Korte Beschrijving', value: 'short_description' },
-        { text: 'Beschrijving', value: 'description' },
-        { text: 'Acties', value: 'actions', sortable: false }
-      ],
-      ratings: [],
-      editedIndex: -1,
-      editedItem: {
-        title: '',
-        short_description: '',
-        description: ''
-      },
-      defaultItem: {
-        title: '',
-        short_description: '',
-        description: ''
-      },
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ]
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      { text: 'Titel', value: 'title' },
+      { text: 'Korte Beschrijving', value: 'short_description' },
+      { text: 'Beschrijving', value: 'description' },
+      { text: 'Actions', value: 'actions', sortable: false }
+    ],
+    ratings: [],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      title: '',
+      short_description: '',
+      description: ''
+    },
+    defaultItem: {
+      title: '',
+      short_description: '',
+      description: ''
     }
-  },
+  }),
+
   computed: {
     formTitle () {
       return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
     }
   },
+
   watch: {
     dialog (val) {
       val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
     }
   },
+
   mounted () {
     axiosInstance
       .get('api/v1/ratings/')
       .then(response => (this.ratings = response.data))
       .catch(error => console.log(error))
   },
+
   methods: {
     editItem (item) {
       this.editedIndex = this.ratings.indexOf(item)
       this.editedItem = Object.assign({}, item)
       this.dialog = true
     },
+
+    deleteItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm () {
+      this.desserts.splice(this.editedIndex, 1)
+      this.closeDelete()
+    },
+
     close () {
       this.dialog = false
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
+    },
+
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.ratings[this.editedIndex], this.editedItem)
+      } else {
+        this.ratings.push(this.editedItem)
+      }
+      this.close()
+    },
+
+    updateForm () {
+      if (this.editedIndex > -1) {
+        axiosInstance.patch(`api/v1/ratings/${this.ratings[this.editedIndex]}`, this.editedItem)
+          .then((response) => {
+          // Perform Success Action
+            console.log(response)
+          })
+          .catch((error) => {
+          // error.response.status Check status code
+            console.log(error)
+          })
+        Object.assign(this.ratings[this.editedIndex], this.editedItem)
+      } else {
+        this.ratings.push(this.editedItem)
+      }
+      this.close()
+    },
+
+    updateRating () {
+      if (this.editedIndex > -1) {
+        dataService.update(this.ratings[this.editedIndex], this.editedItem)
+          .then((response) => {
+            console.log(response.data)
+            this.message = 'The Rating was updated successfully!'
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+      } else {
+        this.ratings.push(this.editedItem)
+      }
     }
   }
 }
