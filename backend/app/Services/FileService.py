8e941316d3b00ce3dd30ef3d397dabc6de86edff
Lisa394 <55276026+Lisa394@users.noreply.github.com>
2022-05-16 17:59:25 +0200

    Save file on disk

--- backend/app/Services/FileService.py
+++ backend/app/Services/FileService.py
@@ -2,11 +2,32 @@ from sqlalchemy.orm import Session
 from Models import FileModel, RevisionModel
 from Repositories import FileRepository
 from fastapi import FastAPI, UploadFile
+import shutil
+from pathlib import Path
 
 
 class FileService:
     @staticmethod
-    def create_file(filename: str, content_type: str, location: str, revision: RevisionModel, db: Session) -> FileModel:
+    def save_file_to_disk(upload_file: UploadFile, location: Path):
+        """create file
+
+        Args:
+            upload_file (object): file that has been uploaded
+            location (str): location of the file
+        Returns:
+            filename (str): name of the file
+            content_type (str): content_type of the file
+            location (str): location of the file
+        """
+        try:
+            with location.open("wb") as buffer:
+                shutil.copyfileobj(upload_file.file, buffer)
+        finally:
+            upload_file.file.close()
+        return{"filename": upload_file.filename, "content_type:": upload_file.content_type, location: Path}
+
+    @staticmethod
+    def create_file_in_db(filename: str, content_type: str, location: str, revision: RevisionModel, db: Session) -> FileModel:
         """create file
 
         Args:
@@ -16,11 +37,6 @@ class FileService:
             revision (RevisionModel): revision file has been added too
             db (Session): database session
         """
-
-        # alright so i gotta make it so a file can be uploaded
-        # i then have to make it so we can get the filename content & loacation
-        # and all of that needs to be the filemodel so it can be saved
-
         file = FileModel(filename=filename, content_type=content_type, location=location, revision=revision)
         file = FileRepository.save(db=db, file=file)
         return file
