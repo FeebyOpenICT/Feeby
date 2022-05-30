aebb4010ec7df7957e554d7363a44a71c3658331
spark-156 <lucabergman@hotmail.nl>
2022-05-30 20:39:13 +0200

    kubernetes fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_secret_from_file(path):
-    with open(path) as secret_file:
+    with open(f'/run/secrets/{path}') as secret_file:
         return secret_file.read()
 
 
@@ -13,16 +13,21 @@ try:
     BASE_APP_API_CALLBACK_URL = f'{BASE_APP_API_URL}/auth/callback'
     JWT_ALGORITHM = os.environ['JWT_ALGORITHM']
     DATABASE_URL = os.environ['DATABASE_URL']
-    CLIENT_ID = get_secret_from_file(os.environ['CANVAS_CLIENT_ID_FILE'])
-    CLIENT_SECRET = get_secret_from_file(
-        os.environ['CANVAS_CLIENT_SECRET_FILE'])
-    JWT_SECRET = get_secret_from_file(os.environ['JWT_SECRET_FILE'])
-    DEVELOPER_KEY = get_secret_from_file(
-        os.environ['CANVAS_DEVELOPER_KEY_FILE'])
-    DELEVOPER_KEY_ID = get_secret_from_file(
-        os.environ['CANVAS_DEVELOPER_KEY_ID_FILE'])
-    POSTGRES_USER = get_secret_from_file(os.environ['POSTGRES_USER_FILE'])
-    POSTGRES_PASSWORD = get_secret_from_file(
-        os.environ['POSTGRES_PASSWORD_FILE'])
+    try:
+        CLIENT_ID = get_secret_from_file('CANVAS_CLIENT_ID')
+        CLIENT_SECRET = get_secret_from_file('CANVAS_CLIENT_SECRET')
+        JWT_SECRET = get_secret_from_file('JWT_SECRET')
+        DEVELOPER_KEY = get_secret_from_file('CANVAS_DEVELOPER_KEY')
+        DELEVOPER_KEY_ID = get_secret_from_file('CANVAS_DEVELOPER_KEY_ID')
+        POSTGRES_USER = get_secret_from_file('POSTGRES_USER')
+        POSTGRES_PASSWORD = get_secret_from_file('POSTGRES_PASSWORD')
+    except:
+        CLIENT_ID = os.environ['CANVAS_CLIENT_ID']
+        CLIENT_SECRET = os.environ['CANVAS_CLIENT_SECRET']
+        JWT_SECRET = os.environ['JWT_SECRET']
+        DEVELOPER_KEY = os.environ['CANVAS_DEVELOPER_KEY']
+        DELEVOPER_KEY_ID = os.environ['CANVAS_DEVELOPER_KEY_ID']
+        POSTGRES_USER = os.environ['POSTGRES_USER']
+        POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 except KeyError as err:
     print(f"Given key not found - {err}")
