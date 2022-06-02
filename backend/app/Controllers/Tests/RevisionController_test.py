0579b943aeaf7509cab129788cf18afefbb94240
Lisa394 <55276026+Lisa394@users.noreply.github.com>
2022-06-02 12:12:55 +0200

    wip tests

--- backend/app/Controllers/Tests/RevisionController_test.py
+++ backend/app/Controllers/Tests/RevisionController_test.py
@@ -1,8 +1,9 @@
 from Models import RevisionModel, PostModel
 from sqlalchemy.orm import Session
 
-""""
+
 def test_create_revision(client, current_active_user, db: Session):
+    post = PostModel("jhdfjskhdf", "jhfkjdshdfkh", user=current_active_user)
     revision = RevisionModel("gsdugfsd", post)
     db.add(revision)
     db.commit()
@@ -11,16 +12,14 @@ def test_create_revision(client, current_active_user, db: Session):
         "files": ""
 
     }
-    response = client.post("/users/{user_id}/files")
+    response = client.post("/users/1/posts/1/revisions/1/files")
     assert response.status_code == 201
-
 
 
 def test_create_revision_without_files(client):
     data = {
         "files": []
     }
-    response = client.post("/users/{user_id}/posts/{post_id}/revisions/{revision_id}/files")
-    assert response.status_code == 201
-"""
+    response = client.post("/users/1/posts/1/revisions/1/files")
+    assert response.status_code == 400
 
