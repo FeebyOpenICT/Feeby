e443c131d944cac643f35568820bf26e267c4097
spark-156 <lucabergman@hotmail.nl>
2022-06-07 12:02:17 +0200

    prettier unauthenticated card

--- frontend/pages/unauthenticated.vue
+++ frontend/pages/unauthenticated.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Not authenticated</div>
+  <v-card>
+    <v-card-title>Not authenticated</v-card-title>
+    <v-card-text
+      >U bent niet ingelogd bij Feeby. Verwijder uw cookies en ga terug naar de
+      canvas omgeving.</v-card-text
+    >
+  </v-card>
 </template>
 
 <script>
