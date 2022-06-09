364d0f14a5fb113ba0eadc41a3a047f71df46ed3
spark-156 <lucabergman@hotmail.nl>
2022-06-09 13:01:18 +0200

    Authorization based on roles per page

--- frontend/middleware/authenticated.js
+++ frontend/middleware/authenticated.js
@@ -1,5 +1,5 @@
-export default function authenticated({ store, redirect }) {
+export default function authenticated({ store, error }) {
   if (!store.getters['auth/isAuthenticated']) {
-    redirect('/unauthenticated')
+    error({ statusCode: 403, message: 'U bent niet ingelogd bij Feeby. Verwijder uw cookies en ga terug naar de canvas omgeving.', title: 'Not authenticated'})
   }
 }
--- frontend/pages/shared.vue
+++ frontend/pages/shared.vue
@@ -1,9 +1,12 @@
 <template>
-  <div></div>
+  <div>Only admins, content-developers and Instructors can view this page</div>
 </template>
 
 <script>
 export default {
-  middleware: ['authenticated'],
+  middleware: ['authenticated', 'authorization'],
+  meta: {
+    roles: ['admin', 'content_developer', 'instructor']
+  }
 }
 </script>
