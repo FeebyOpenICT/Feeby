fa537e2b80a23fe2ca7488dface5272af9635222
lucius02 <54938206+lucius02@users.noreply.github.com>
2022-06-13 15:10:07 +0200

    fix

--- frontend/pages/aspects-ratings.vue
+++ frontend/pages/aspects-ratings.vue
@@ -1,15 +1,15 @@
 <template>
   <v-row>
     <v-col> </v-col>
-    <v-col> <AspectsInzien /> </v-col>
+    <v-col> <AspectsBeheren /> </v-col>
   </v-row>
 </template>
 
 <script>
-import AspectsInzien from '~/components/AspectsInzien.vue'
+import AspectsBeheren from '~/components/AspectsBeheren.vue'
 export default {
   name: 'aspects-ratings',
   middleware: ['authenticated'],
-  components: { AspectsInzien },
+  components: { AspectsBeheren },
 }
 </script>
