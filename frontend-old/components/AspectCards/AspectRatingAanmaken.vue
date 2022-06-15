d7906978e4d80a69a4aeba7fc8a233f06578c0e0
timrebergen <60223267+timrebergen@users.noreply.github.com>
2022-06-15 15:24:15 +0200

    Post revision (#39)
    * StudentCreatePost_oude_post_call
    
    De Create Post pagina met oude post call. CSS moet nog gefixed worden.
    
    * Fix missing permissions
    
    * Eerste versie Revision_post_12-05-22
    
    Dit is de eerste versie van de revision + post pagina. De post pagina is functioneel. De revision pagina zelf moet nog gecodeerd worden.
    
    * Werkende Post request met User_id
    
    De user_id wordt nu goed opgevraagd bij het maken van een nieuwe post.
    
    * create does not belong to exception
    
    * create post with initial revision and feedback
    
    * refactor postcontroller
    
    * create revisions
    
    * WIP give feedback on revision
    
    * CSS_changes_tekst_beschrijving_+_toevoeging_uploadbox
    
    Ik heb een aantal stijlelementen van de titel en beschrijving aangepast, zodat deze nu op een leesbare manier aansluiten op de huisstijl van Feeby. Ook heb ik een uploadbox toegevoegd. Deze heeft drag en drop functionaliteit. Echter werkt dit laatste nog niet helemaal.
    
    * Uploadbox_toegevoegd
    
    In deze versie is er een uploadbox toegevoegd. Daarnaast heb ik ook een extra eindscherm toegevoegd, waarin de eindgebruiker het totale overzicht van zijn/haar ingeleverde werk nog eens terug kan zien.
    
    * Kleine update
    
    Kleine update
    
    * Feedback_aanpassingen_beroepsproductpagina
    
    In deze versie heb ik de titel van de post veranderd van revision naar productinleveren. Daarnaast zijn er ook vele CSS feedback aanpassingen doorgevoerd.
    
    * checking baseline measurement
    
    * Aanpassingen_17-05
    
    Aanpassingen_17-05
    
    * get rid of unused import
    
    * Folder_aanpassingen_+_product_inleveren_en_revision_toegevoegd
    
    Voor deze update heb ik de folderstructuur in deze branch aangepast. Daarnaast heb ik ook nog de aspecten ophalen toegevoegd in de productinleveren pagina.
    
    * Aspecten_toevoeging
    
    Aspecten_toevoeging
    
    * Kleine_aanpassingen_code
    
    * Aanpassingen_props
    
    * Revert "Aanpassingen_props"
    
    This reverts commit b12aaecb260d6e2d05b6382397a6fd9dd8aa0f27.
    
    * Revert "Merge branch 'PostRevision' of https://github.com/FeebyOpenICT/Feeby into PostRevision"
    
    This reverts commit 5ead8f0eeee06493e63520c73fd8ae17ff10d026, reversing
    changes made to 94cdebadab7d75231e65314c56e9650adeafed21.
    
    * add comments
    
    * test
    
    * Revert "Merge branch 'PostRevision' of https://github.com/FeebyOpenICT/Feeby into PostRevision"
    
    This reverts commit 5ead8f0eeee06493e63520c73fd8ae17ff10d026, reversing
    changes made to 94cdebadab7d75231e65314c56e9650adeafed21.
    
    * Revert "test"
    
    This reverts commit 7ce806d16f903538e7b8a96bdede0d154a8c35b3.
    
    * Werkende_selectbox_aspecten_inladen
    
    Het aanvinken van de ingeladen aspecten op de postinleveren pagina, werkt nu naar behoren. In de direct opvolgende versie staat het uploaden van files en specifieke aspecten selecteren, om dit vervolgens allemaal te verzenden als post, op het programma.
    
    * Test_vuex
    
    * Revert "Test_vuex"
    
    This reverts commit 3052173e18ba0201f994d127dad5b1e0fa2bb7bc.
    
    * Textarea_test
    
    * Revert_aspectlist
    
    * Aspecten_in_parent_comp_gezet
    
    Ik heb ervoor gekozen om de aspecten in de parent component neer te zetten. De child component en het inladen hiervan werkt momenteel nog niet naar behoren. Ik heb inmiddels hulp gevraagd aan externe partijen of zij mij/ons hiermee in te toekomst kunnen helpen.
    
    * Update_ratings
    
    * Revision_post_test
    
    Revision_post_test
    
    * Nieuwe frontend
    
    * New front-end productinleveren
    
    * Werkende_selectie_productinleveren
    
    Het selecteren van aspecten tijdens het inleveren van een beroepsproduct wordt getoond in de payload.
    
    * Description (aspect_id + rating_id)
    
    * Get_users_aangepast
    
    * Werkende ratings + select
    
    * Werkende beroepsproductinleveren pagina. Uploaden werkt nog niet.
    
    * delete node_modules
    
    * remove unused files
    
    * remove unwanted file
    
    Co-authored-by: spark-156 <lucabergman@hotmail.nl>

--- frontend-old/components/AspectCards/AspectRatingAanmaken.vue
+++ frontend-old/components/AspectCards/AspectRatingAanmaken.vue
@@ -19,7 +19,7 @@
           max. 255 characters
         </div>
       </div>
-      <div id="productTextContainer" class="container">
+      <div class="productTextContainer">
         <h2 class="textBoxTitle">
           Beschrijving
         </h2>
@@ -33,11 +33,11 @@
           placeholder="Schrijf hier je beschrijving over dit aspect rating..."
         />
 
-        <div id="characterLimit">
+        <div class="characterLimit">
           max. 1000 characters
         </div>
       </div>
-      <div id="productTextContainer" class="container">
+      <div class="productTextContainer">
         <h2 class="textBoxTitle">
           Korte Beschrijving
         </h2>
@@ -51,7 +51,7 @@
           placeholder="Schrijf hier je korte beschrijving over dit aspect rating..."
         />
 
-        <div id="characterLimit">
+        <div class="characterLimit">
           max. 255 characters
         </div>
       </div>
---
+++ frontend-old/pages/Productinleveren.vue
@@ -0,0 +1,14 @@
+<template className="container">
+  <ProductPage />
+</template>
+
+<script>
+import ProductPage from '~/components/Product.vue'
+// import HeaderCom from './HeaderCom.vue'
+// import FooterCom from './FooterCom.vue'
+
+export default {
+  name: 'BeroepsproductInleveren',
+  components: { ProductPage }
+}
+</script>
