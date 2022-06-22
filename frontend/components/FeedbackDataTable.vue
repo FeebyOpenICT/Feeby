da1dd9cc7bf033edf98e1b340daa54a5af447f33
spark-156 <lucabergman@hotmail.nl>
2022-06-22 10:53:26 +0200

    Opened post

---
+++ frontend/components/FeedbackDataTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-data-table
+    hide-default-footer
+    disable-pagination
+    :headers="headers"
+    :items="feedback"
+    :items-per-page="-1"
+  >
+    <template
+      v-slot:item.reviewer="{ item }"
+    >
+      <AvatarAndName class="pa-n4" :canvas_email="item.reviewer.canvas_email" :fullname="item.reviewer.fullname"/>
+    </template>
+  </v-data-table>
+</template>
+
+<script lang="ts">
+import Vue, {PropType} from "vue";
+import {Feedback} from "~/types/GetPostByID";
+
+export default Vue.extend({
+  name: "FeedbackDataTable",
+  props: {
+    feedback: {
+      type: Array as PropType<Feedback[]>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Reviewer',
+          align: 'start',
+          sortable: false,
+          value: 'reviewer',
+        },
+        {text: 'Aspect', sortable: false, value: 'aspect.title'},
+        {text: 'Rating', sortable: false, value: 'rating.title'},
+        {text: 'Review', sortable: false, value: 'description'},
+      ],
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/components/Revision.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-timeline-item
+    fill-dot
+    class="white--text mb-12"
+    small
+  >
+    <template v-slot:icon>
+      <span>{{ index + 1 }}</span>
+    </template>
+    <v-card flat>
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-subtitle>{{ formattedCreationTime }}</v-card-subtitle>
+      <v-card-text>
+        {{ revision.description }}
+      </v-card-text>
+      <feedback-data-table :feedback="feedback"/>
+    </v-card>
+  </v-timeline-item>
+</template>
+
+<script lang="ts">
+import Vue, {PropType} from "vue";
+import {Feedback, Revision} from "~/types/GetPostByID";
+import {DateTime} from "luxon";
+
+export default Vue.extend({
+  name: "Revision",
+  props: {
+    revision: {
+      type: Object as PropType<Revision>,
+      required: true
+    },
+    title: String,
+    index: Number,
+    feedback: {
+      type: Array as PropType<Feedback[]>,
+      required: true
+    }
+  },
+  computed: {
+    formattedCreationTime() {
+      return DateTime.fromISO(this.revision.time_created).toLocaleString({
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+      })
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
